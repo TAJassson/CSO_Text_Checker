f9b6373031d574600721bf600dc24189f227d48d
Jason Tse <despa.tse@gmail.com>
2023-04-07 08:28:42 +0800

    加入專案檔案。

---
+++ CSO_Text_Checker/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace CSO_Text_Checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DisplayFileName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rtbErrors = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // DisplayFileName
+            // 
+            this.DisplayFileName.Location = new System.Drawing.Point(12, 12);
+            this.DisplayFileName.Name = "DisplayFileName";
+            this.DisplayFileName.Size = new System.Drawing.Size(870, 23);
+            this.DisplayFileName.TabIndex = 0;
+            this.DisplayFileName.TextChanged += new System.EventHandler(this.DisplayFileName_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(888, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            // 
+            // rtbErrors
+            // 
+            this.rtbErrors.Location = new System.Drawing.Point(12, 41);
+            this.rtbErrors.Name = "rtbErrors";
+            this.rtbErrors.Size = new System.Drawing.Size(977, 881);
+            this.rtbErrors.TabIndex = 2;
+            this.rtbErrors.Text = "";
+            this.rtbErrors.TextChanged += new System.EventHandler(this.rtbErrors_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 934);
+            this.Controls.Add(this.rtbErrors);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DisplayFileName);
+            this.Name = "Form1";
+            this.Text = "CSO Language file checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox DisplayFileName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox rtbErrors;
+    }
+}
---
+++ CSO_Text_Checker/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CSO_Text_Checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            rtbErrors.Clear();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select File";
+            openFileDialog.InitialDirectory = @"C:";//--"C:\";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.ShowDialog();
+            if (openFileDialog.FileName != "")
+            {
+                DisplayFileName.Text = openFileDialog.FileName;
+            }
+            else
+            {
+                DisplayFileName.Text = "You didn't select the file!";
+            }
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] lines = File.ReadAllLines(openFileDialog.FileName);
+                int batchSize = 100;
+                int numBatches = (int)Math.Ceiling((double)lines.Length / batchSize);
+                int errorCount = 0;
+
+                for (int i = 0; i < numBatches; i++)
+                {
+                    int start = i * batchSize;
+                    int end = Math.Min(start + batchSize, lines.Length);
+
+                    for (int j = start; j < end; j++)
+                    {
+                        if (string.IsNullOrWhiteSpace(lines[j]) || lines[j].StartsWith("{") || lines[j].StartsWith("}") || lines[j].StartsWith("//")|| lines[j].StartsWith("/") || lines[j].Contains("/") || lines[j].Contains("//") || lines[j].Contains("Tokens") || lines[j].Contains("}") || lines[j].Contains("{") || lines[j].Contains("Language"))
+                        {
+                            continue;
+                        }
+
+                        int quoteCount = 0;
+                        for (int k = 0; k < lines[j].Length; k++)
+                        {
+                            if (lines[j][k] == '"')
+                            {
+                                quoteCount++;
+                                if (quoteCount == 4)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                        if (quoteCount != 4)
+                        {
+                            rtbErrors.AppendText(string.Format("Line {0}: {1}{2}", j + 1, lines[j], Environment.NewLine));
+                            for (int k = 0; k < lines[j].Length; k++)
+                            {
+                                if (lines[j][k] == '"')
+                                {
+                                }
+                                else
+                                {
+                                }
+                            }
+                            rtbErrors.AppendText(Environment.NewLine);
+                            errorCount++;
+                        }
+                    }
+                }
+
+                MessageBox.Show(string.Format("Finished processing {0} lines with {1} errors.", lines.Length, errorCount));
+            }
+        }
+
+        private void DisplayFileName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rtbErrors_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
